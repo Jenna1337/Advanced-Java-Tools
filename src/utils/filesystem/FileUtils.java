780ebee949b7d8eef8c83f550c1f1a43362e089d
Jenna Sloan <jenna3715@gmail.com>
2020-06-15 03:44:48 -0500

    added readChunks
    readChunks basically does the same thing as copyChunks, except it's more efficient, and passes the bytes read to a consumer instead of creating new files.

--- src/utils/filesystem/FileUtils.java
+++ src/utils/filesystem/FileUtils.java
@@ -1,6 +1,7 @@
 package utils.filesystem;
 
 import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -9,7 +10,9 @@ import java.io.StringReader;
 import java.nio.file.AccessDeniedException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Consumer;
 import utils.StringUtils;
+import utils.collections.LimitedSizeDeque;
 
 public class FileUtils
 {
@@ -61,7 +64,7 @@ public class FileUtils
 	public static File[] copyChunks(final File source, String outputdir,
 			String ext, final String chunk_start, final String chunk_end,
 			final int adat_pre, final int adat_post, final boolean overwrite)
-			throws AccessDeniedException, IOException
+					throws AccessDeniedException, IOException
 	{
 		// TODO add functionality for adat_pre
 		ArrayList<File> files = new ArrayList<File>();
@@ -159,5 +162,98 @@ public class FileUtils
 		}
 		sfi.close();
 		return files.toArray(new File[files.size()]);
+	}
+	/**
+	 * TODO: test this method<br>
+	 * <br>
+	 * Reads and passes the desired chunks of data from a file to a given
+	 * consumer.<br>
+	 * <b>Note:</b> large chunks will take <u>longer</u> to process, and
+	 * <u>there is no way to know how much more time is required</u>.
+	 * 
+	 * @param source The {@link File} to read from.
+	 * @param outputdir The directory to write to.
+	 * @param chunk_start The {@code byte[]} that denotes the start of a chunk.
+	 * @param chunk_end The {@code byte[]} that denotes the end of the chunk.
+	 * @param adat_pre Number of bytes before {@code chunk_end}.
+	 * @param adat_post Number of bytes after {@code chunk_end}.
+	 * @return The files written.
+	 * @throws IOException if an I/O error occurs.
+	 */
+	public static void readChunks(final File source, 
+			final Consumer<byte[]> consumer, final byte[] chunk_start, 
+			final byte[] chunk_end, final int adat_pre, final int adat_post)
+					throws AccessDeniedException, IOException
+	{
+		LimitedSizeDeque<Byte> pre_bytes = new LimitedSizeDeque<Byte>(adat_pre);
+		ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+		Consumer<Byte> clearMainBufferAndAddByteToPreChunkBytesDeque = (b)->{
+			buffer.write(0);
+			pre_bytes.add(b);
+		};
+		Consumer<Byte> readCheckStartAppendHelper = (b)->{
+			buffer.write(b);
+			pre_bytes.add(b);
+		};
+		int b;
+		boolean successfullyReadChunkStart = false,
+				successfullyReadChunkEnd = false;
+		
+		try(BufferedInputStream sfi = new BufferedInputStream(new FileInputStream(source)))
+		{
+			loop0: while(true){
+				buffer.reset();
+				
+				successfullyReadChunkStart = readChunksHelperMethod1(
+						sfi, chunk_start,
+						readCheckStartAppendHelper,
+						clearMainBufferAndAddByteToPreChunkBytesDeque,
+						true);
+				if(!successfullyReadChunkStart)
+					break;
+				
+				successfullyReadChunkEnd = readChunksHelperMethod1(
+						sfi, chunk_end,
+						buffer::write,
+						buffer::write,
+						false);
+				if(!successfullyReadChunkEnd)
+					break;
+				
+				//read adat_post more bytes to buffer
+				for(int i=0;i<adat_post;++i){
+					if(((b = sfi.read())==-1))
+						break loop0;
+					buffer.write(b);
+				}
+				
+				consumer.accept(buffer.toByteArray());
+				
+				break;
+			}
+		sfi.close();
+		}
+		catch(IOException e){
+			throw new AccessDeniedException(source.getAbsolutePath(), null, e.getMessage());
+		}
+	}
+	private static boolean readChunksHelperMethod1(BufferedInputStream sfi,
+			final byte[] toFindAndRead, final Consumer<Byte> whatToDoIfValid,
+			final Consumer<Byte> whatToDoIfNotValid,
+			final boolean isBeginningChunk) throws IOException{
+		int b,i=0;
+		while(i<toFindAndRead.length && (b = sfi.read())!=-1){
+			if(b == toFindAndRead[i]){
+				++i;
+				whatToDoIfValid.accept((byte)b);
+			}
+			else{
+				i=0;
+				whatToDoIfNotValid.accept((byte)b);
+			}
+		}
+		// TODO remember what isBeginningChunk was for
+		
+		return true;
 	}
 }
